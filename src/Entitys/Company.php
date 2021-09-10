88aad2e4555e55a781c97111187c76db6e82da40
Павел Сучилов <suchilov@f5.com.ru>
2021-09-10 18:03:21 +0300

    Добавил остальные сущности

---
+++ src/Entitys/Company.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jarvis\AmoApi\Entitys;
+
+class Company extends Entity
+{
+
+    protected static string $path = '/api/v4/companies';
+    protected static string $entity_type = 'companies';
+
+}
---
+++ src/Entitys/Task.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jarvis\AmoApi\Entitys;
+
+class Task extends Entity
+{
+
+    protected static string $path = '/api/v4/tasks';
+    protected static string $entity_type = 'tasks';
+
+}
